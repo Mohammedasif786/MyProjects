2045f368e458d0c16657e124d3496942c30fd43f
asif <formohammedasif6@gmail.com>
2023-10-31 22:48:06 -0300

    I want to check student subject

--- Project_cpp/records.hpp
+++ Project_cpp/records.hpp
@@ -49,9 +49,24 @@ void Records() {
         //Logic Code...
         cout << "Enter student roll number :: ";
         cin >> student[i].rollnumber;
+        cout << "Name of Student :: ";
+        cin >> student[i].student_name;
+
+        short int subject_loop = 0;
+
+        do  {
+           cout << student[subject_loop].subjects << " : ";
+           cin >> student[subject_loop].marks[subject_loop];
+           ++subject_loop; 
+        } while (subject_loop<5);
+        
     
 
         //++count;
     }
+    for(int i=0;i<5;++i) {
+        cout << student[i].subjects << ": " << student[i].marks[i] << endl;
+    }
+
     //cout << "The number of times :: " << count << endl;
 }
