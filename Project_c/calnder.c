b991a3a72e2f5e19dfd5d6f32f91d94d4c62b886
asif <formohammedasif6@gmail.com>
2024-02-15 11:53:48 -0800

    Complete Calander

--- Project_c/calnder.c
+++ Project_c/calnder.c
@@ -1,109 +1,112 @@
 #include<stdio.h>
-#include "calstr.h"
-#define true 1
-
-int main(void) {
-    struct calender c1;
-    *(c1.days+1) = "MON";
-    unsigned jan = 31,feb = 29,month,mar = 31,apr = 30,may = 30,jun = 30,jul = 31,Aug = 31,sep = 30,oct = 31,nov = 30,dec = 31;
-    int user = 1;
-
-    for (size_t i = 0; i < size; i++)
-        c1.cal[i] = i+1;
-    
-    //printf("\tSUN \tMON \tTUE \tWED \tTHU \tFRI \tSAT\n");
-
-    while(true) {
-        printf("1.Jan\n");
-        printf("2.Feb\n");
-        printf("3.Mar\n");
-        printf("4.Apr\n");
-        printf("5.May\n");
-        printf("6.Jun\n");
-        printf("7.Jul\n");
-        printf("8.Aug\n");
-        printf("9.Sep\n");
-        printf("10.Oct\n");
-        printf("11.Nov\n");
-        printf("12.Dec\n");
-        printf("13.Show all the Calnder\n");
-        printf("Enter you chocie :: ");
-        scanf("%d",&user);
-        switch (user)
-        {
-        case 1:
-            month = jan;
-            break;
-        
-        case 2:
-            month = feb;
-            break;
-
-        case 3:
-            month = mar;
-            break;
-
-        case 4:
-            month = apr;
-            break;
-
-        case 5:
-            month = may;
-            break;
-
-        case 6:
-            month = jun;
-            break;
-
-        case 7:
-            month = jul;
-            break;
-
-         case 8:
-            month = Aug;
-            break;
-
-                case 9:
-            month = sep;
-            break;
-                 case 10:
-            month = oct;
-            break;
-                  
-         case 11:
-            month = nov;
-            break;
-            
-         case 12:
-            month = dec;
-            break;
-
-        case 13:
-        break;
-        
-        default:
-            break;
-        }
-
-    printf("\tSUN \tMON \tTUE \tWED \tTHU \tFRI \tSAT\n");
-    for (size_t i = 0,j = 0,k = 0; i < size; i++,j++,++k)
+#include<conio.h>
+#include<stdlib.h>
+int main(int argc,char** argv)
+{
+    int m,h,i=1,a,j,b=1;
+    printf("\nEnter Month number of the year 2022:");
+    scanf("%d",&m);
+    switch (m)
     {
-       if(j == 7) {
-        printf("\n");
-        j = 0;
-       }
-       
-        if (k == month) {
-            k = 0;
-            printf("\n");
-         printf("\tSUN \tMON \tTUE \tWED \tTHU \tFRI \tSAT\n");
-       }
-       
-       printf("\t %u ",c1.cal[k]);
+    case 1:
+        a=7;
+        j=31;
+        break;
+    case 2:
+        a=3;
+        j=28;
+        break;
+    case 3:
+        a=3;
+        j=31;
+        break;
+    case 4:
+        a=6;
+        j=30;
+        break;
+    case 5:
+        a=1;
+        j=31;
+        break;
+    case 6:
+        a=4;
+        j=30;
+        break;
+    case 7:
+        a=6;
+        j=31;
+        break;
+    case 8:
+        a=2;
+        j=31;
+        break;
+    case 9:
+        a=5;
+        j=30;
+        break;
+    case 10:
+        a=7;
+        j=31;
+        break;
+    case 11:
+        a=3;
+        j=30;
+        break;
+    case 12:
+        a=5;
+        j=31;
+        break;
+    default:
+    printf("\a\a Invalid month");
+    exit(0);
+ 
     }
-    break;
+    printf("\n\n\n");
+    printf("\t\t\t Month - %d -2015 \n\n",m);
+    printf("       SUN       MON       TUE      THU      FRI        SAT \n\n",m);
+    switch(a)
+    {
+    case 1:
+        printf("\t%d",i);
+        break;
+    case 2:
+        printf("\t\t%d",i);
+        break;
+    case 3:
+        printf("\t\t\t%d",i);
+        break;
+    case 4:
+        printf("\t\t\t\t%d",i);
+        break;
+    case 5:
+        printf("\t\t\t\t\t%d",i);
+        break;
+    case 6:
+        printf("\t\t\t\t\t\t%d",i);
+        break;
+    case 7:
+        printf("\t\t\t\t\t\t\t%d",i);
+        break;
+   
     }
-    //* 48 spaces contain!
+    h=8-a;
+    for(i=2;i<=h;i++)
+    {
+        printf("\t%d",i);
 
+
+    }
+    printf("\n");
+    for(i=h+1;i<=j;i++)
+    {
+        if(b==8)
+        {
+            printf("\n");
+            b=1;
+        }
+        printf("\t%d",i);
+        b++;
+    }
+    getch();
     return 0;
 }
